ace6641df20e33cddcbc9dda4f5af8b95933d781
wassim <wbvb41@gmail.com>
2023-01-25 02:44:27 +0100

    learned how to work with the map method on arrays

--- script.js
+++ script.js
@@ -83,3 +83,6 @@ const currencies = new Map([
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const eurToUsd = 1.1;
+// const movementsUsd = movements.map(mov => mov * eurToUsd);
+// console.log(movementsUsd);
